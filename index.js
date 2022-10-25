9e6233730bcc5892900e3601ede90c41ae56f540
atique-atq <atique.atq@gmail.com>
2022-10-25 21:56:50 +0600

    started json file

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const course = require('./data/course.json');
+const courseDetails = require('./data/courseDetails.json');
+
+app.get('/', (req, res) => {
+    res.send('News API Running');
+})
+
+app.get('/course', (req, res) => {
+    res.send(course);
+});
+
+app.get('/course/:id', (req, res) => {
+    const details = courseDetails.filter(n => n.category_id === id);
+    res.send(details)
+})
+
+app.listen(port, () => {
+    console.log('Dragon News Server Running on Port', port);
+})
